8ed3f1189c778610a8f98289973a8891eb89765d
zhengkang <zhengkang@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2017-04-28 09:43:40 +0000

    改成test了。。。

--- DEV_KW/yqyd_xs/app/src/main/java/com/zk/myweex/activity/TestActivity.java
+++ DEV_KW/yqyd_xs/app/src/main/java/com/zk/myweex/activity/TestActivity.java
@@ -3,6 +3,7 @@ package com.zk.myweex.activity;
 import android.os.Bundle;
 
 import com.taobao.weex.utils.WXFileUtils;
+import com.zk.myweex.WXApplication;
 
 public class TestActivity extends WXBaseActivity {
 
@@ -10,7 +11,12 @@ public class TestActivity extends WXBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        renderPage(WXFileUtils.loadAsset("index.weex.js", this), "", "");
+//        renderPage(WXFileUtils.loadAsset("yqyd/dist/tab0.js", this), "", "");
+
+        String path = WXApplication.PATH + "yqydTab0.zip/yqyd/dist/tab0.js";
+        renderPage(WXFileUtils.readFileInZip(path),
+                "file://" + WXApplication.PATH + "yqydTab0.zip/yqyd/dist/",
+                "yqydTab0.zip");
     }
 
 }
