049e0e8175960637500dd5834ce510e9291c4d06
zhengkang <zhengkang@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2017-05-17 01:39:46 +0000

    wxvideo 改动。

--- yqyd_xs/sdk/src/main/java/com/taobao/weex/ui/view/WXVideoView.java
+++ yqyd_xs/sdk/src/main/java/com/taobao/weex/ui/view/WXVideoView.java
@@ -217,6 +217,7 @@ import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.VideoView;
@@ -228,240 +229,256 @@ import com.taobao.weex.utils.WXResourceUtils;
 
 public class WXVideoView extends VideoView implements WXGestureObservable {
 
-  private WXGesture wxGesture;
-  private VideoPlayListener mVideoPauseListener;
+    private WXGesture wxGesture;
+    private VideoPlayListener mVideoPauseListener;
 
-  public WXVideoView(Context context) {
-    super(context);
-  }
-
-  @Override
-  public void registerGestureListener(WXGesture wxGesture) {
-    this.wxGesture = wxGesture;
-  }
-
-  public void setOnVideoPauseListener(VideoPlayListener listener) {
-    mVideoPauseListener = listener;
-  }
-
-  @Override
-  public boolean onTouchEvent(MotionEvent event) {
-    boolean result = super.onTouchEvent(event);
-    if (wxGesture != null) {
-      result |= wxGesture.onTouch(this, event);
-    }
-    return result;
-  }
-
-  @Override
-  public void start() {
-    super.start();
-    if (mVideoPauseListener != null) {
-      mVideoPauseListener.onStart();
-    }
-  }
-
-  @Override
-  public void pause() {
-    super.pause();
-    if (mVideoPauseListener != null) {
-      mVideoPauseListener.onPause();
-    }
-  }
-
-  public interface VideoPlayListener {
-
-    void onPause();
-
-    void onStart();
-  }
-
-
-  public static class Wrapper extends FrameLayout implements ViewTreeObserver.OnGlobalLayoutListener {
-
-    private WXVideoView mVideoView;
-    private ProgressBar mProgressBar;
-    private MediaController mMediaController;
-    private Uri mUri;
-    private MediaPlayer.OnPreparedListener mOnPreparedListener;
-    private MediaPlayer.OnErrorListener mOnErrorListener;
-    private MediaPlayer.OnCompletionListener mOnCompletionListener;
-    private WXVideoView.VideoPlayListener mVideoPlayListener;
-
-
-    public Wrapper(Context context) {
-      super(context);
-      init(context);
-    }
-
-    public Wrapper(Context context, AttributeSet attrs) {
-      super(context, attrs);
-      init(context);
-    }
-
-    public Wrapper(Context context, AttributeSet attrs, int defStyleAttr) {
-      super(context, attrs, defStyleAttr);
-      init(context);
-    }
-
-    private void init(Context context) {
-      setBackgroundColor(WXResourceUtils.getColor("#ee000000"));
-      mProgressBar = new ProgressBar(context);
-      FrameLayout.LayoutParams pLayoutParams =
-          new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
-              FrameLayout.LayoutParams.WRAP_CONTENT);
-      mProgressBar.setLayoutParams(pLayoutParams);
-      pLayoutParams.gravity = Gravity.CENTER;
-      addView(mProgressBar);
-
-      getViewTreeObserver().addOnGlobalLayoutListener(this);
-    }
-
-    public ProgressBar getProgressBar() {
-      return mProgressBar;
-    }
-
-    public
-    @Nullable
-    WXVideoView getVideoView() {
-      return mVideoView;
-    }
-
-    /**
-     * Create if not existed. Will cause request focus.
-     *
-     * @return
-     */
-    public
-    @NonNull
-    WXVideoView createIfNotExist() {
-      if (mVideoView == null) {
-        createVideoView();
-      }
-      return mVideoView;
-    }
-
-    public
-    @Nullable
-    MediaController getMediaController() {
-      return mMediaController;
-    }
-
-    public void setVideoURI(Uri uri) {
-      mUri = uri;
-      if (mVideoView != null) {
-        mVideoView.setVideoURI(uri);
-      }
-    }
-
-    public void start() {
-      if (mVideoView != null) {
-        mVideoView.start();
-      }
-    }
-
-    public void pause() {
-      if (mVideoView != null) {
-        mVideoView.pause();
-      }
-    }
-
-    public void stopPlayback() {
-      if (mVideoView != null) {
-        mVideoView.stopPlayback();
-      }
-    }
-
-    public void resume() {
-      if (mVideoView != null) {
-        mVideoView.resume();
-      }
-    }
-
-    public void setOnErrorListener(MediaPlayer.OnErrorListener l) {
-      mOnErrorListener = l;
-      if (mVideoView != null) {
-        mVideoView.setOnErrorListener(l);
-      }
-    }
-
-    public void setOnPreparedListener(MediaPlayer.OnPreparedListener l) {
-      mOnPreparedListener = l;
-      if (mVideoView != null) {
-        mVideoView.setOnPreparedListener(l);
-      }
-
-    }
-
-    public void setOnCompletionListener(MediaPlayer.OnCompletionListener l) {
-      mOnCompletionListener = l;
-      if (mVideoView != null) {
-        mVideoView.setOnCompletionListener(l);
-      }
-    }
-
-    public void setOnVideoPauseListener(VideoPlayListener listener) {
-      mVideoPlayListener = listener;
-      if (mVideoView != null) {
-        mVideoView.setOnVideoPauseListener(listener);
-      }
-    }
-
-    private synchronized void createVideoView() {
-      if(mVideoView != null){
-        return;
-      }
-      Context context = getContext();
-      WXVideoView video = new WXVideoView(context);
-      FrameLayout.LayoutParams videoLayoutParams =
-          new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
-              FrameLayout.LayoutParams.MATCH_PARENT);
-      videoLayoutParams.gravity = Gravity.CENTER;
-      video.setLayoutParams(videoLayoutParams);
-      addView(video, 0);//first child
-      video.setOnErrorListener(mOnErrorListener);
-      video.setOnPreparedListener(mOnPreparedListener);
-      video.setOnCompletionListener(mOnCompletionListener);
-      video.setOnVideoPauseListener(mVideoPlayListener);
-      MediaController controller = new MediaController(context);
-      controller.setAnchorView(this);
-      video.setMediaController(controller);
-      controller.setMediaPlayer(video);
-
-      mMediaController = controller;
-      mVideoView = video;
-
-      if(mUri != null) {
-        setVideoURI(mUri);
-      }
-    }
-
-    @SuppressLint("NewApi")
-    private void removeSelfFromViewTreeObserver() {
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-        getViewTreeObserver().removeOnGlobalLayoutListener(this);
-      } else {
-        getViewTreeObserver().removeGlobalOnLayoutListener(this);
-      }
-    }
-
-
-    public boolean createVideoViewIfVisible(){
-      Rect visibleRect = new Rect();
-      if (mVideoView != null) {
-        return true;
-      } else if (getGlobalVisibleRect(visibleRect) && !visibleRect.isEmpty()) {
-        createVideoView();
-        return true;
-      }
-      return false;
+    public WXVideoView(Context context) {
+        super(context);
     }
 
     @Override
-    public void onGlobalLayout() {
-      if(createVideoViewIfVisible()){
-        removeSelfFromViewTreeObserver();
-      }
+    public void registerGestureListener(WXGesture wxGesture) {
+        this.wxGesture = wxGesture;
     }
-  }
+
+    public void setOnVideoPauseListener(VideoPlayListener listener) {
+        mVideoPauseListener = listener;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean result = super.onTouchEvent(event);
+        if (wxGesture != null) {
+            result |= wxGesture.onTouch(this, event);
+        }
+        return result;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        if (mVideoPauseListener != null) {
+            mVideoPauseListener.onStart();
+        }
+    }
+
+    @Override
+    public void pause() {
+        super.pause();
+        if (mVideoPauseListener != null) {
+            mVideoPauseListener.onPause();
+        }
+    }
+
+    public interface VideoPlayListener {
+
+        void onPause();
+
+        void onStart();
+    }
+
+
+    public static class Wrapper extends FrameLayout implements ViewTreeObserver.OnGlobalLayoutListener {
+
+        private WXVideoView mVideoView;
+        private ProgressBar mProgressBar;
+        private ImageView mImageView;
+        private MediaController mMediaController;
+        private Uri mUri;
+        private MediaPlayer.OnPreparedListener mOnPreparedListener;
+        private MediaPlayer.OnErrorListener mOnErrorListener;
+        private MediaPlayer.OnCompletionListener mOnCompletionListener;
+        private WXVideoView.VideoPlayListener mVideoPlayListener;
+
+
+        public Wrapper(Context context) {
+            super(context);
+            init(context);
+        }
+
+        public Wrapper(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            init(context);
+        }
+
+        public Wrapper(Context context, AttributeSet attrs, int defStyleAttr) {
+            super(context, attrs, defStyleAttr);
+            init(context);
+        }
+
+        private void init(Context context) {
+            setBackgroundColor(WXResourceUtils.getColor("#ee000000"));
+            mProgressBar = new ProgressBar(context);
+            FrameLayout.LayoutParams pLayoutParams =
+                    new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
+                            FrameLayout.LayoutParams.WRAP_CONTENT);
+            mProgressBar.setLayoutParams(pLayoutParams);
+            pLayoutParams.gravity = Gravity.CENTER;
+            addView(mProgressBar);
+
+
+            mImageView = new ImageView(context);
+//            mImageView.setBackgroundResource(R.drawable.bg_video);
+            FrameLayout.LayoutParams ivLayoutParams =
+                    new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
+                            FrameLayout.LayoutParams.WRAP_CONTENT);
+            mImageView.setLayoutParams(ivLayoutParams);
+            pLayoutParams.gravity = Gravity.CENTER;
+            addView(mImageView);
+
+
+            getViewTreeObserver().addOnGlobalLayoutListener(this);
+        }
+
+        public ProgressBar getProgressBar() {
+            return mProgressBar;
+        }
+
+        public ImageView getImageView() {
+            return mImageView;
+        }
+
+        public
+        @Nullable
+        WXVideoView getVideoView() {
+            return mVideoView;
+        }
+
+        /**
+         * Create if not existed. Will cause request focus.
+         *
+         * @return
+         */
+        public
+        @NonNull
+        WXVideoView createIfNotExist() {
+            if (mVideoView == null) {
+                createVideoView();
+            }
+            return mVideoView;
+        }
+
+        public
+        @Nullable
+        MediaController getMediaController() {
+            return mMediaController;
+        }
+
+        public void setVideoURI(Uri uri) {
+            mUri = uri;
+            if (mVideoView != null) {
+                mVideoView.setVideoURI(uri);
+            }
+        }
+
+        public void start() {
+            if (mVideoView != null) {
+                mVideoView.start();
+            }
+        }
+
+        public void pause() {
+            if (mVideoView != null) {
+                mVideoView.pause();
+            }
+        }
+
+        public void stopPlayback() {
+            if (mVideoView != null) {
+                mVideoView.stopPlayback();
+            }
+        }
+
+        public void resume() {
+            if (mVideoView != null) {
+                mVideoView.resume();
+            }
+        }
+
+        public void setOnErrorListener(MediaPlayer.OnErrorListener l) {
+            mOnErrorListener = l;
+            if (mVideoView != null) {
+                mVideoView.setOnErrorListener(l);
+            }
+        }
+
+        public void setOnPreparedListener(MediaPlayer.OnPreparedListener l) {
+            mOnPreparedListener = l;
+            if (mVideoView != null) {
+                mVideoView.setOnPreparedListener(l);
+            }
+
+        }
+
+        public void setOnCompletionListener(MediaPlayer.OnCompletionListener l) {
+            mOnCompletionListener = l;
+            if (mVideoView != null) {
+                mVideoView.setOnCompletionListener(l);
+            }
+        }
+
+        public void setOnVideoPauseListener(VideoPlayListener listener) {
+            mVideoPlayListener = listener;
+            if (mVideoView != null) {
+                mVideoView.setOnVideoPauseListener(listener);
+            }
+        }
+
+        private synchronized void createVideoView() {
+            if (mVideoView != null) {
+                return;
+            }
+            Context context = getContext();
+            WXVideoView video = new WXVideoView(context);
+            FrameLayout.LayoutParams videoLayoutParams =
+                    new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                            FrameLayout.LayoutParams.MATCH_PARENT);
+            videoLayoutParams.gravity = Gravity.CENTER;
+            video.setLayoutParams(videoLayoutParams);
+            addView(video, 0);//first child
+            video.setOnErrorListener(mOnErrorListener);
+            video.setOnPreparedListener(mOnPreparedListener);
+            video.setOnCompletionListener(mOnCompletionListener);
+            video.setOnVideoPauseListener(mVideoPlayListener);
+            MediaController controller = new MediaController(context);
+            controller.setAnchorView(this);
+            video.setMediaController(controller);
+            controller.setMediaPlayer(video);
+
+            mMediaController = controller;
+            mVideoView = video;
+
+            if (mUri != null) {
+                setVideoURI(mUri);
+            }
+        }
+
+        @SuppressLint("NewApi")
+        private void removeSelfFromViewTreeObserver() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            } else {
+                getViewTreeObserver().removeGlobalOnLayoutListener(this);
+            }
+        }
+
+
+        public boolean createVideoViewIfVisible() {
+            Rect visibleRect = new Rect();
+            if (mVideoView != null) {
+                return true;
+            } else if (getGlobalVisibleRect(visibleRect) && !visibleRect.isEmpty()) {
+                createVideoView();
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public void onGlobalLayout() {
+            if (createVideoViewIfVisible()) {
+                removeSelfFromViewTreeObserver();
+            }
+        }
+    }
 }
