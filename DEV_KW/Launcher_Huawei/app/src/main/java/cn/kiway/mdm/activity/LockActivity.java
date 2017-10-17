8e06b3d8ffb6af83ab6490a39b1bda90a474672b
zhengkang <zhengkang@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2017-10-17 01:02:00 +0000

    准备加入推送

--- DEV_KW/Launcher_Huawei/app/src/main/java/cn/kiway/mdm/activity/LockActivity.java
+++ DEV_KW/Launcher_Huawei/app/src/main/java/cn/kiway/mdm/activity/LockActivity.java
@@ -1,12 +1,18 @@
 package cn.kiway.mdm.activity;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.kiway.mdm.R;
+import cn.kiway.mdm.broadcast.SampleDeviceReceiver;
+import cn.kiway.mdm.mdm.MDMHelper;
 
 /**
  * Created by Administrator on 2017/10/13.
@@ -37,11 +43,13 @@ public class LockActivity extends BaseActivity {
             textView.setText("学生模式");
             moshi.setText("当前模式为：家长模式");
             Toast.makeText(this, "解锁成功", Toast.LENGTH_SHORT).show();
+            unlock();
         } else {
             getSharedPreferences("kiway", 0).edit().putBoolean("locked", true).commit();
             textView.setText("家长模式");
             moshi.setText("当前模式为：学生模式");
             Toast.makeText(this, "锁定成功", Toast.LENGTH_SHORT).show();
+            lock();
         }
     }
 
@@ -52,5 +60,55 @@ public class LockActivity extends BaseActivity {
     public void Before(View view) {
         finish();
     }
+
+    private void lock() {
+        //初始化adapter
+        ComponentName mAdminName = new ComponentName(this, SampleDeviceReceiver.class);
+        MDMHelper.getAdapter().init(this, mAdminName);
+        //1.设置默认桌面
+        MDMHelper.getAdapter().setDefaultLauncher("cn.kiway.mdm", "cn.kiway.mdm.activity.MainActivity");
+        //2.关闭settings.慎用！！！
+        //MDMHelper.getAdapter().setSettingsApplicationDisabled(true);
+        //3.设置不可卸载
+        List<String> packages = new ArrayList<>();
+        packages.add("cn.kiway.mdm");
+        MDMHelper.getAdapter().addDisallowedUninstallPackages(packages);
+        MDMHelper.getAdapter().addPersistentApp(packages);
+        //4.禁止下拉状态栏
+        MDMHelper.getAdapter().setStatusBarExpandPanelDisabled(true);
+        //5.禁止USB，慎用！！！
+        MDMHelper.getAdapter().setUSBDataDisabled(true);
+        //6.禁用一些物理键盘
+        MDMHelper.getAdapter().setTaskButtonDisabled(true);
+        MDMHelper.getAdapter().setHomeButtonDisabled(true);
+        //MDMHelper.getAdapter().setVpnDisabled(true); //这个失效。
+        //7.禁止修改时间
+        //MDMHelper.getAdapter().setTimeAndDateSetDisabled(true);//这个失效
+    }
+
+    private void unlock() {
+        //初始化adapter
+        ComponentName mAdminName = new ComponentName(this, SampleDeviceReceiver.class);
+        MDMHelper.getAdapter().init(this, mAdminName);
+        //1.设置默认桌面
+        MDMHelper.getAdapter().clearDefaultLauncher();
+        //2.关闭settings.慎用！！！
+        //MDMHelper.getAdapter().setSettingsApplicationDisabled(true);
+        //3.设置不可卸载
+        List<String> packages = new ArrayList<>();
+        packages.add("cn.kiway.mdm");
+        MDMHelper.getAdapter().addDisallowedUninstallPackages(packages);
+        MDMHelper.getAdapter().addPersistentApp(packages);
+        //4.禁止下拉状态栏
+        MDMHelper.getAdapter().setStatusBarExpandPanelDisabled(false);
+        //5.禁止USB，慎用！！！
+        MDMHelper.getAdapter().setUSBDataDisabled(false);
+        //6.禁用一些物理键盘
+        MDMHelper.getAdapter().setTaskButtonDisabled(false);
+        MDMHelper.getAdapter().setHomeButtonDisabled(false);
+        //MDMHelper.getAdapter().setVpnDisabled(true); //这个失效。
+        //MDMHelper.getAdapter().setTimeAndDateSetDisabled(false);//这个失效
+    }
+
 
 }
--- DEV_KW/Launcher_Huawei/app/src/main/java/cn/kiway/mdm/activity/MainActivity.java
+++ DEV_KW/Launcher_Huawei/app/src/main/java/cn/kiway/mdm/activity/MainActivity.java
@@ -50,131 +50,12 @@ public class MainActivity extends BaseActivity implements CheckPassword.CheckPas
         group = (LinearLayout) findViewById(R.id.points);
         //1.设置初始密码
         String password = getSharedPreferences("kiway", 0).getString("password", "");
-        Log.d("test", "pasword = " + password);
         if (TextUtils.isEmpty(password)) {
             dialog.setTitle("请设置初始密码");
             dialog.setCancelable(false);
             dialog.show();
-//            final EditText et = new EditText(this);
-//            et.setText("123456");
-//            et.setSelection(et.getText().toString().length());
-//            et.setSingleLine();
-//            new AlertDialog.Builder(this).setTitle("请设置初始密码").setIcon(
-//                    android.R.drawable.ic_dialog_info).setView(et
-//            ).setPositiveButton("确认", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialog, int which) {
-//                    String input = et.getText().toString();
-//                    if (input.equals("")) {
-//                        toast("密码不能为空");
-//                        return;
-//                    }
-//                    getSharedPreferences("kiway", 0).edit().putString("password", input).commit();
-//                }
-//            }).setCancelable(false).show();
         }
         initData();
-    }
-
-    public void clickButton2(View v) {
-        if (getSharedPreferences("kiway", 0).getBoolean("locked", false)) {
-            toast("当前已锁定");
-            return;
-        }
-        toast("开始锁定");
-        getSharedPreferences("kiway", 0).edit().putBoolean("locked", true).commit();
-
-        //初始化adapter
-        ComponentName mAdminName = new ComponentName(this, SampleDeviceReceiver.class);
-        MDMHelper.getAdapter().init(this, mAdminName);
-        //1.设置默认桌面
-        MDMHelper.getAdapter().setDefaultLauncher("cn.kiway.mdm", "cn.kiway.mdm.activity.MainActivity");
-        //2.关闭settings.慎用！！！
-        //MDMHelper.getAdapter().setSettingsApplicationDisabled(true);
-        //3.设置不可卸载
-        List<String> packages = new ArrayList<>();
-        packages.add("cn.kiway.mdm");
-        MDMHelper.getAdapter().addDisallowedUninstallPackages(packages);
-        MDMHelper.getAdapter().addPersistentApp(packages);
-        //4.禁止下拉状态栏
-        MDMHelper.getAdapter().setStatusBarExpandPanelDisabled(true);
-        //5.禁止USB，慎用！！！
-        MDMHelper.getAdapter().setUSBDataDisabled(true);
-        //6.禁用一些物理键盘
-        MDMHelper.getAdapter().setTaskButtonDisabled(true);
-        MDMHelper.getAdapter().setHomeButtonDisabled(true);
-        //MDMHelper.getAdapter().setVpnDisabled(true); 这个失效。
-        //7.禁止修改时间
-        MDMHelper.getAdapter().setTimeAndDateSetDisabled(true);
-    }
-
-    public void clickButton3(View v) {
-        //进入APP列表
-        if (!getSharedPreferences("kiway", 0).getBoolean("locked", false)) {
-            toast("请先锁定再进入学习园地");
-            return;
-        }
-        startActivity(new Intent(this, AppListActivity.class));
-    }
-
-    public void clickButton4(View v) {
-        if (!getSharedPreferences("kiway", 0).getBoolean("locked", false)) {
-            toast("请先锁定再进入其他应用");
-            return;
-        }
-        startActivity(new Intent(this, AppListActivity2.class));
-    }
-
-    public void clickButton5(View v) {
-        dialog.setView(null, 0);
-        dialog.setCancelable(true);
-        dialog.setTitle("请输入密码");
-        dialog.show();
-//        //弹出密码框，密码正确就解锁
-//        final EditText et = new EditText(this);
-//        et.setText("123456");
-//        et.setSingleLine();
-//        new AlertDialog.Builder(this).setTitle("请输入密码").setIcon(
-//                android.R.drawable.ic_dialog_info).setView(et
-//        ).setPositiveButton("确认", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                String input = et.getText().toString();
-//                if (input.equals("")) {
-//                    toast("密码不能为空");
-//                    return;
-//                }
-//                String password = getSharedPreferences("kiway", 0).getString("password", "");
-//                if (!input.equals(password)) {
-//                    toast("密码错误");
-//                    return;
-//                }
-//                toast("已经解锁");
-//                getSharedPreferences("kiway", 0).edit().putBoolean("locked", false).commit();
-//
-//                //初始化adapter
-//                ComponentName mAdminName = new ComponentName(MainActivity.this, SampleDeviceReceiver.class);
-//                MDMHelper.getAdapter().init(MainActivity.this, mAdminName);
-//                //1.设置默认桌面
-//                MDMHelper.getAdapter().clearDefaultLauncher();
-//                //2.关闭settings.慎用！！！
-//                //MDMHelper.getAdapter().setSettingsApplicationDisabled(true);
-//                //3.设置不可卸载
-//                List<String> packages = new ArrayList<>();
-//                packages.add("cn.kiway.mdm");
-//                MDMHelper.getAdapter().addDisallowedUninstallPackages(packages);
-//                MDMHelper.getAdapter().addPersistentApp(packages);
-//                //4.禁止下拉状态栏
-//                MDMHelper.getAdapter().setStatusBarExpandPanelDisabled(false);
-//                //5.禁止USB，慎用！！！
-//                MDMHelper.getAdapter().setUSBDataDisabled(false);
-//                //6.禁用一些物理键盘
-//                MDMHelper.getAdapter().setTaskButtonDisabled(false);
-//                MDMHelper.getAdapter().setHomeButtonDisabled(false);
-//                //MDMHelper.getAdapter().setVpnDisabled(true); 这个失效。
-//
-//            }
-//        }).show();
     }
 
     public void Camera(View view) {
@@ -199,35 +80,9 @@ public class MainActivity extends BaseActivity implements CheckPassword.CheckPas
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        Intent intent = new Intent(Intent.ACTION_VIEW);//发送短信的界面
-//        intent.putExtra("address", "");
-//        intent.putExtra("sms_body", "");
-//        intent.setType("vnd.android-dir/mms-sms");
-//        startActivity(intent);
     }
 
     public void ChangePassWord(View view) {
-//        final EditText et = new EditText(this);
-//        et.setText("123456");
-//        et.setSingleLine();
-//        new AlertDialog.Builder(this).setTitle("请输入密码").setIcon(
-//                android.R.drawable.ic_dialog_info).setView(et
-//        ).setPositiveButton("确认", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                String input = et.getText().toString();
-//                if (input.equals("")) {
-//                    toast("密码不能为空");
-//                    return;
-//                }
-//                String password = getSharedPreferences("kiway", 0).getString("password", "");
-//                if (!input.equals(password)) {
-//                    toast("密码错误");
-//                    return;
-//                }
-//                startActivity(new Intent(MainActivity.this, ChangePassWordActivity.class));
-//            }
-//        }).show();
         dialog.setView(null, 1);
         dialog.setCancelable(true);
         dialog.setTitle("请输入密码");
@@ -327,7 +182,7 @@ public class MainActivity extends BaseActivity implements CheckPassword.CheckPas
             });//每一个GridView作为一个View对象添加到ViewPager集合中
             viewPagerList.add(gridView);
         }
-        viewPager.setPageTransformer(false,new StereoPagerTransformer());
+        viewPager.setPageTransformer(false, new StereoPagerTransformer());
         viewPager.setAdapter(new MyViewPagerAdapter(viewPagerList));//设置ViewPager适配器
         group.removeAllViews();
         ivPoints = new ImageView[totalPage];//添加小圆点
@@ -354,5 +209,13 @@ public class MainActivity extends BaseActivity implements CheckPassword.CheckPas
                 }
             }
         });
+    }
+
+    public void clickButton4(View v) {
+        if (!getSharedPreferences("kiway", 0).getBoolean("locked", false)) {
+            toast("请先锁定再进入其他应用");
+            return;
+        }
+        startActivity(new Intent(this, AppListActivity2.class));
     }
 }
