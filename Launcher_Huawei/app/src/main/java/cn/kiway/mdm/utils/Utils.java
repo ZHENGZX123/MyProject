eedad9da63079353c1167a880a762de68d734c22
zhengkang <zhengkang@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2017-11-06 03:49:21 +0000

    imei=null

--- Launcher_Huawei/app/src/main/java/cn/kiway/mdm/utils/Utils.java
+++ Launcher_Huawei/app/src/main/java/cn/kiway/mdm/utils/Utils.java
@@ -413,8 +413,39 @@ public class Utils {
     public static String getIMEI(Context c) {
         TelephonyManager tm = (TelephonyManager) c.getSystemService(Context.TELEPHONY_SERVICE);
         String imei = tm.getDeviceId();
+        if (TextUtils.isEmpty(imei)) {
+            imei = c.getSharedPreferences("kiway", 0).getString("imei", "");
+            if (TextUtils.isEmpty(imei)) {
+                imei = genIMEI();
+                c.getSharedPreferences("kiway", 0).edit().putString("imei", imei).commit();
+            }
+        }
         Log.d("test", "IMEI = " + imei);
         return imei;
+    }
+
+    public static String genIMEI() {// calculator IMEI
+        int r1 = 1000000 + new java.util.Random().nextInt(9000000);
+        int r2 = 1000000 + new java.util.Random().nextInt(9000000);
+        String input = r1 + "" + r2;
+        char[] ch = input.toCharArray();
+        int a = 0, b = 0;
+        for (int i = 0; i < ch.length; i++) {
+            int tt = Integer.parseInt(ch[i] + "");
+            if (i % 2 == 0) {
+                a = a + tt;
+            } else {
+                int temp = tt * 2;
+                b = b + temp / 10 + temp % 10;
+            }
+        }
+        int last = (a + b) % 10;
+        if (last == 0) {
+            last = 0;
+        } else {
+            last = 10 - last;
+        }
+        return input + last;
     }
 
     public static void uploadLocation(final MainActivity c, final double longitude, final double latitude) {
