17a76c073266b80dc4091193129b812f0ee0f33d
zhengkang <zhengkang@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2017-11-27 01:59:17 +0000

    0.0.4发布

--- DEV_KW/MDM_Parent/app/src/main/java/cn/kiway/mdm/util/Utils.java
+++ DEV_KW/MDM_Parent/app/src/main/java/cn/kiway/mdm/util/Utils.java
@@ -5,6 +5,9 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Message;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.Log;
 
 import cn.kiway.mdm.activity.MainActivity;
 
@@ -65,6 +68,44 @@ public class Utils {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static String getIMEI(Context c) {
+        TelephonyManager tm = (TelephonyManager) c.getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = tm.getDeviceId();
+        if (TextUtils.isEmpty(imei)) {
+            imei = c.getSharedPreferences("kiway", 0).getString("imei", "");
+            if (TextUtils.isEmpty(imei)) {
+                imei = genIMEI();
+                c.getSharedPreferences("kiway", 0).edit().putString("imei", imei).commit();
+            }
+        }
+        Log.d("test", "IMEI = " + imei);
+        return imei;
+    }
+
+    public static String genIMEI() {// calculator IMEI
+        int r1 = 1000000 + new java.util.Random().nextInt(9000000);
+        int r2 = 1000000 + new java.util.Random().nextInt(9000000);
+        String input = r1 + "" + r2;
+        char[] ch = input.toCharArray();
+        int a = 0, b = 0;
+        for (int i = 0; i < ch.length; i++) {
+            int tt = Integer.parseInt(ch[i] + "");
+            if (i % 2 == 0) {
+                a = a + tt;
+            } else {
+                int temp = tt * 2;
+                b = b + temp / 10 + temp % 10;
+            }
+        }
+        int last = (a + b) % 10;
+        if (last == 0) {
+            last = 0;
+        } else {
+            last = 10 - last;
+        }
+        return input + last;
     }
 
 }
