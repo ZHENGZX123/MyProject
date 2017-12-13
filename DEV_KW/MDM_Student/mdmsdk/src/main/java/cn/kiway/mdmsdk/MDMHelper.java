26d76b3930f2a321862bfb0248ec2b0be041e204
zhengkang <zhengkang@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2017-12-13 04:21:01 +0000

    集成华瑞安方案

--- DEV_KW/MDM_Student/mdmsdk/src/main/java/cn/kiway/mdmsdk/MDMHelper.java
+++ DEV_KW/MDM_Student/mdmsdk/src/main/java/cn/kiway/mdmsdk/MDMHelper.java
@@ -1,6 +1,7 @@
 package cn.kiway.mdmsdk;
 
 import android.os.Build;
+import android.util.Log;
 
 /**
  * Created by Administrator on 2017/9/1.
@@ -13,8 +14,11 @@ public class MDMHelper {
     public static IMDMAdapter getAdapter() {
         if (mAdapter == null) {
             //根据平板类型来选择用哪个Adapter
+            Log.d("test", "Model = " + Build.MODEL);
             if (Build.MODEL.equals("rk3288")) {
                 mAdapter = new JiChengShiXunAdapter();
+            } else if (Build.MODEL.equals("D13B")) {
+                mAdapter = new HuaruianMDMAdapter();
             } else {
                 mAdapter = new HuaweiMDMAdapter();
             }
