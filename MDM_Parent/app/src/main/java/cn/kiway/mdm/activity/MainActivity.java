f92c66622c83c2247d18514128063a6f4ccb56ab
zhengkang <zhengkang@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2017-12-15 02:22:33 +0000

    改改图标

--- MDM_Parent/app/src/main/java/cn/kiway/mdm/activity/MainActivity.java
+++ MDM_Parent/app/src/main/java/cn/kiway/mdm/activity/MainActivity.java
@@ -69,7 +69,7 @@ import static cn.kiway.mdm.util.Utils.getCurrentVersion;
 
 
 public class MainActivity extends BaseActivity {
-    private static final String currentPackageVersion = "0.1.0";
+    private static final String currentPackageVersion = "0.1.1";
 
     private boolean isSuccess = false;
     private boolean isJump = false;
