a4efaf5d9da57971b803658307684c523af6d2f6
zhengkang <zhengkang@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2018-01-03 01:40:47 +0000

    fix ui

--- MDM_KTHD/app/src/main/java/cn/kiway/mdm/activity/LoginActivity.java
+++ MDM_KTHD/app/src/main/java/cn/kiway/mdm/activity/LoginActivity.java
@@ -10,9 +10,12 @@ import studentsession.kiway.cn.mdm_studentsession.R;
  */
 
 public class LoginActivity extends BaseActivity {
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
 }
