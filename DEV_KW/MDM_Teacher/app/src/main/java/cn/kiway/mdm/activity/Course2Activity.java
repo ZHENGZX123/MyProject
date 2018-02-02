42293d58799b317f88de797a59c7c1ed8defb831
zhengzaoxin <zhengzaoxin@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2018-02-02 08:42:09 +0000

    Failed commit: Default (2)

--- DEV_KW/MDM_Teacher/app/src/main/java/cn/kiway/mdm/activity/Course2Activity.java
+++ DEV_KW/MDM_Teacher/app/src/main/java/cn/kiway/mdm/activity/Course2Activity.java
@@ -16,7 +16,6 @@ public class Course2Activity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course2);
-
         initView();
     }
 
