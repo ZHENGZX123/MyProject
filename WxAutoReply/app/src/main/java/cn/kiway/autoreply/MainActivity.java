46905ebef66c52aa8427f8bea09267271d095354
zhengkang <zhengkang@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2018-03-23 01:12:56 +0000

    还是用回我的版本

--- WxAutoReply/app/src/main/java/cn/kiway/autoreply/MainActivity.java
+++ WxAutoReply/app/src/main/java/cn/kiway/autoreply/MainActivity.java
@@ -4,32 +4,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.text.TextUtils;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
+
+import static cn.kiway.autoreply.Action.TYPE_TEST;
 
 public class MainActivity extends Activity {
 
-    private EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = (EditText) findViewById(R.id.name);
 
     }
 
     public void start(View view) {
-        String nameStr = name.getText().toString();
-        if (TextUtils.isEmpty(nameStr)) {
-            toast("名字不能为空");
-            return;
-        }
         try {
-            toast("选择开维客服机器人-打开");
+            toast("选择“开维客服机器人“-选择“打开”");
             Intent accessibleIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
             startActivity(accessibleIntent);
         } catch (Exception e) {
@@ -37,8 +30,17 @@ public class MainActivity extends Activity {
         }
     }
 
+    public void logout(View view) {
+
+    }
+
+
     public void test(View v) throws Exception {
-        AutoReplyService.instance.doSendMsg("hello" + System.currentTimeMillis());
+        Action a = new Action();
+        a.sender = "test";
+        a.content = "content";
+        a.receiveType = TYPE_TEST;
+        AutoReplyService.instance.sendMsgToServer(9999, a);
     }
 
     public void toast(final String txt) {
