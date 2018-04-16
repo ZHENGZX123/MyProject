ba55d98f9568f3be2aee0466a411a42a742a3cac
zhengkang <zhengkang@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2018-04-16 02:52:48 +0000

    改回队列

---
+++ DEV_KW/KwRobot/app/src/main/java/cn/kiway/robot/entity/ZbusRecv.java
@@ -0,0 +1,17 @@
+package cn.kiway.robot.entity;
+
+/**
+ * Created by Administrator on 2018/4/16.
+ */
+
+public class ZbusRecv {
+
+    public String msg;
+    public boolean realReply;
+
+
+    public ZbusRecv(String msg, boolean realReply) {
+        this.msg = msg;
+        this.realReply = realReply;
+    }
+}
