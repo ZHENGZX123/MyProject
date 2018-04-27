2a8614c3850f8f05769c0effbe75be4f170c071b
zhengkang <zhengkang@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2018-04-27 09:31:30 +0000

    H·K隆興行星光商业广场2009

---
+++ DEV_KW/KwRobot/app/src/main/java/cn/kiway/robot/moment/model/Zombie.java
@@ -0,0 +1,15 @@
+package cn.kiway.robot.moment.model;
+
+/**
+ * Created by Administrator on 2018/4/27.
+ */
+
+public class Zombie {
+    public String nickname;
+    public boolean cleared;
+
+    public Zombie(String nickname, boolean clear) {
+        this.nickname = nickname;
+        this.cleared = cleared;
+    }
+}
