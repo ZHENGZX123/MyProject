a91fb402c2c63782b13c97869b20c8cbff991b38
zhengkang <zhengkang@dbfd10c2-de4d-554c-9009-ce568fefba2b>
2018-05-25 10:18:39 +0000

    准备发正式0.2.0

--- KwRobot/app/src/main/java/cn/kiway/robot/util/Constant.java
+++ KwRobot/app/src/main/java/cn/kiway/robot/util/Constant.java
@@ -26,13 +26,13 @@ public class Constant {
 
     static {
 
-//        host = "robot.kiway.cn";
-//        port = 5676;
-//        clientUrl = "http://robot.kiway.cn";
+        host = "robot.kiway.cn";
+        port = 5676;
+        clientUrl = "http://robot.kiway.cn";
 
-        host = "rbtest.kiway.cn";
-        port = 5672;
-        clientUrl = "http://rbtest.kiway.cn";
+//        host = "rbtest.kiway.cn";
+//        port = 5672;
+//        clientUrl = "http://rbtest.kiway.cn";
 
     }
 
